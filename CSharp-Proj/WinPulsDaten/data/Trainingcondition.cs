24895e55778b3a409b9a93f07e7a4792d026c20b
Noah <1055840@bkrnet.de>
2022-03-22 08:58:10 +0100

    Fixed but with Trainingcondition, removed gender-table from database and implemented hills-calulations
    + Hills-Calulations
    - Gender-Table

--- CSharp-Proj/WinPulsDaten/data/Trainingcondition.cs
+++ CSharp-Proj/WinPulsDaten/data/Trainingcondition.cs
@@ -9,6 +9,10 @@ namespace WinPulsDaten.data
 
         public override string ToString() => this.Name;
 
+        // Hardcoded values to check for different conditions
+        public bool IsOverweight => this.ID == 3;
+        public bool IsTrained => this.ID == 1;
+        public bool IsUntrained => this.ID == 2;
 
         /// <summary>
         /// Method to load a training-condition from a Datarow that got retreived from a database
